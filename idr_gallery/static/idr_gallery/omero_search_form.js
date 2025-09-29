f59b0ba5c9dd217ed3da74a08580832f2d8c1a0c
William Moore <w.moore@dundee.ac.uk>
2025-09-29 14:49:58 +0100

    Remove search results link to open in BFF

--- idr_gallery/static/idr_gallery/omero_search_form.js
+++ idr_gallery/static/idr_gallery/omero_search_form.js
@@ -321,10 +321,9 @@ async function getAutoCompleteResults(key, query, knownKeys, operator) {
 
 const SPINNER_SVG = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sync" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-sync fa-w-16 fa-spin fa-lg"><path fill="currentColor" d="M440.65 12.57l4 82.77A247.16 247.16 0 0 0 255.83 8C134.73 8 33.91 94.92 12.29 209.82A12 12 0 0 0 24.09 224h49.05a12 12 0 0 0 11.67-9.26 175.91 175.91 0 0 1 317-56.94l-101.46-4.86a12 12 0 0 0-12.57 12v47.41a12 12 0 0 0 12 12H500a12 12 0 0 0 12-12V12a12 12 0 0 0-12-12h-47.37a12 12 0 0 0-11.98 12.57zM255.83 432a175.61 175.61 0 0 1-146-77.8l101.8 4.87a12 12 0 0 0 12.57-12v-47.4a12 12 0 0 0-12-12H12a12 12 0 0 0-12 12V500a12 12 0 0 0 12 12h47.35a12 12 0 0 0 12-12.6l-4.15-82.57A247.17 247.17 0 0 0 255.83 504c121.11 0 221.93-86.92 243.55-201.82a12 12 0 0 0-11.8-14.18h-49.05a12 12 0 0 0-11.67 9.26A175.86 175.86 0 0 1 255.83 432z" class=""></path></svg>`;
 class OmeroSearchForm {
-  constructor(formId, SEARCH_ENGINE_URL, resultsId, BASE_URL, SUBMITQUERY_AS_BFF) {
+  constructor(formId, SEARCH_ENGINE_URL, resultsId, BASE_URL) {
     this.SEARCH_ENGINE_URL = SEARCH_ENGINE_URL;
     this.BASE_URL = BASE_URL;
-    this.SUBMITQUERY_AS_BFF = SUBMITQUERY_AS_BFF;
     this.resources_data = {};
     this.query_mode = "searchterms";
     this.cached_key_values = {};
@@ -821,26 +820,6 @@ class OmeroSearchForm {
     });
   }
 
-  getBffUrl(studyName) {
-    // The data_url is SUBMITQUERY_AS_BFF with query parameters
-    let data_url = `${this.SUBMITQUERY_AS_BFF}`;
-
-    let query = this.getPreviousSearchQuery();
-    // NB: only handle first AND clause
-    let and_0 = query.query_details.and_filters[0];
-    data_url = `${data_url}?key=${and_0.name}&value=${and_0.value}&operator=${and_0.operator}&container=${studyName}`;
-    return data_url;
-    // This is the source object for BioFile Finder
-    // let source = {
-    //     "uri": data_url,
-    //     "type": "csv",
-    //     "name": `${studyName}.csv`,
-    // }
-    // let s = encodeURIComponent(JSON.stringify(source));
-    // let bff_url = `${BFF_URL}?source=${s}`;
-    // return bff_url;
-  }
-
   displayResults(data) {
     let studyList = data.results?.results || [];
 
@@ -887,8 +866,6 @@ class OmeroSearchForm {
               <div style="margin-left: 20px">
                 Images from study: <a href="${BASE_URL}study/${tokens[0]}/" title="Open Study landing page">
                   ${tokens[0]}</a>
-                | Open ${count} images in
-                <a href="${this.getBffUrl(studyName)}" target="_blank" title="Open search results from ${studyId} in BioFile Finder">BioFile Finder</a>
               </div>
               <ul></ul>
               <div class="studyImagesSpinner">
